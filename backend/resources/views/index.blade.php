264c9534f61ace3a9a59da37155f73814e011c26
simtrami <me@simtrami.net>
2025-03-14 17:02:01 +0100

    Form d'event moins moche

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -7,10 +7,12 @@
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="styles.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link rel="stylesheet" href="flatpickr.min.css">
+    <script src="flatpickr.js"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
-    <div style="display: flex; flex-direction: column; align-items: center;">
+    <div class="flex flex-col items-center">
         <div id="timeline"></div>
     </div>
     <script src="https://d3js.org/d3.v7.min.js"></script>
