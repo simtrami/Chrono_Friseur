a1c4bdcaca33ab71b005f84de0ccddaefbaf8f10
simtrami <me@simtrami.net>
2025-04-15 17:29:30 +0200

    Add search events flyout
    With fields to filter by text, tags and date.

--- resources/views/components/timeline.blade.php
+++ resources/views/components/timeline.blade.php
@@ -320,11 +320,19 @@
                         this.$dispatch('notify', { content: `Une erreur s'est produite lors de l'ajout.`, type: 'error' });
                     }
                 }).finally(() => { this.tagRequestInProgress[this.formTag.id] = false; })
-        }
+        },
+        openSearchFlyout: false,
+        showSearch(event) {
+            // Remove focus on the button because the focus must not be hidden from assistive technology users.
+            document.activeElement.blur();
+            this.openSearchFlyout = true;
+            this.mode = 'search';
+        },
     }"
     @timeline-select.window="showEvent($event)"
     @add-event.window="showAddForm($event)"
     @list-tags.window="showListTags($event)"
+    @open-search.window="showSearch($event)"
     class="w-full h-full flex items-center bg-white"
 >
     <!-- Loading overlay -->
@@ -334,8 +342,89 @@
 
     <!-- Event flyout -->
     <x-flyout x-model="openEventFlyout">
+        <!-- mode === showEvent -->
         <x-events.show x-show="selectedEvent.id && mode === 'showEvent'"/>
+
+        <div x-show="mode === 'showEvent'" class="mt-6 flex justify-end space-x-2">
+            <button @click.prevent="showEditEvent()" type="button"
+                    class="relative flex items-center justify-center space-x-1 whitespace-nowrap rounded-lg border border-transparent bg-transparent px-3 py-2 font-semibold text-gray-800 hover:bg-gray-800/10 transition"
+            >
+                <x-icons.pencil-square size="size-5"/>
+
+                <span>Modifier</span>
+            </button>
+
+            <button @click.prevent="deleteEvent()" type="button"
+                    class="relative flex items-center justify-center space-x-1 whitespace-nowrap rounded-lg border border-transparent px-3 py-2 text-white font-semibold bg-red-600 outline-0 outline-transparent hover:bg-red-500 focus:outline-2 focus:outline-offset-2 focus:outline-red-700 transition"
+                    :class="{'opacity-50 cursor-not-allowed': eventRequestInProgress, 'animate-wiggle': !preventEventDelete}"
+                    :disabled="eventRequestInProgress"
+            >
+                <template x-if="preventEventDelete && !eventRequestInProgress">
+                <x-icons.trash size="size-5"/>
+                </template>
+
+                <template x-if="eventRequestInProgress">
+                <x-icons.spinner size="size-5"/>
+                </template>
+
+                <template x-if="!eventRequestInProgress && !preventEventDelete">
+                <x-icons.face-frown size="size-5"/>
+                </template>
+
+                <span x-show="!eventRequestInProgress"
+                      x-text="preventEventDelete ? 'Supprimer' : 'Vraiment ?'"
+                ></span>
+            </button>
+        </div>
+
+        <!-- Mode === editEvent || addEvent -->
         <x-events.form x-show="(formEvent.id && mode === 'editEvent') || mode === 'addEvent'"/>
+
+        <!-- Actions for editEvent -->
+        <div x-show="mode === 'editEvent'" class="mt-6 flex justify-end space-x-2">
+            <button @click.prevent="cancelEditEvent()" type="button"
+                    class="relative flex items-center justify-center space-x-1 whitespace-nowrap rounded-lg border border-transparent bg-transparent px-3 py-2 font-semibold text-gray-800 hover:bg-gray-800/10 transition"
+            ><span>Annuler</span></button>
+
+            <button @click.prevent="updateEvent()" type="button"
+                    class="relative flex items-center justify-center space-x-1 whitespace-nowrap rounded-lg border border-transparent px-3 py-2 text-white font-semibold bg-indigo-600 outline-0 outline-transparent hover:bg-indigo-500 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-700 transition"
+                    :class="{'opacity-50 cursor-not-allowed': eventRequestInProgress}"
+                    :disabled="eventRequestInProgress"
+            >
+                <template x-if="!eventRequestInProgress">
+                <x-icons.pencil-square size="size-5"/>
+                </template>
+
+                <template x-if="eventRequestInProgress">
+                <x-icons.spinner size="size-5"/>
+                </template>
+
+                <span>Appliquer</span>
+            </button>
+        </div>
+
+        <!-- Actions for addEvent -->
+        <div x-show="mode === 'addEvent'" class="mt-6 flex justify-end space-x-2">
+            <button @click.prevent="$dialog.close()" type="button"
+                    class="relative flex items-center justify-center space-x-1 whitespace-nowrap rounded-lg border border-transparent bg-transparent px-3 py-2 font-semibold text-gray-800 hover:bg-gray-800/10 transition"
+            ><span>Annuler</span></button>
+
+            <button @click.prevent="addEvent()" type="button"
+                    class="relative flex items-center justify-center space-x-1 whitespace-nowrap rounded-lg border border-transparent px-3 py-2 text-white font-semibold bg-indigo-600 outline-0 outline-transparent hover:bg-indigo-500 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-700 transition"
+                    :class="{'opacity-50 cursor-not-allowed': eventRequestInProgress}"
+                    :disabled="eventRequestInProgress"
+            >
+                <template x-if="!eventRequestInProgress">
+                <x-icons.plus size="size-5"/>
+                </template>
+
+                <template x-if="eventRequestInProgress">
+                <x-icons.spinner size="size-5"/>
+                </template>
+
+                <span>Ajouter</span>
+            </button>
+        </div>
     </x-flyout>
 
     <!-- Tag flyout -->
@@ -343,7 +432,20 @@
         <x-tags.list/>
     </x-flyout>
 
+    <!-- Search flyout -->
+    <x-flyout x-model="openSearchFlyout">
+        <x-search/>
+    </x-flyout>
+
     <div class="fixed flex space-x-4 bottom-0 right-0 pr-8 pb-8 z-10 md:pr-12 md:pb-12">
+        <!-- Open search button -->
+        <button x-tooltip="'Rechercher'"
+                @click="$dispatch('open-search')" type="button"
+                class="whitespace-nowrap h-min rounded-full bg-slate-100 p-3 text-base font-semibold text-indigo-500 shadow hover:shadow-xl hover:bg-slate-50 outline-0 outline-transparent focus:outline-2 focus:outline-offset-2 focus:outline-indigo-700 transition"
+        >
+            <x-icons.search size="size-6"/>
+        </button>
+
         <!-- List tags button -->
         <button x-tooltip="'Gestion des tags'"
                 @click="$dispatch('list-tags')" type="button"
