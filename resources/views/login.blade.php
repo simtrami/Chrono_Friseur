d9c6511b695350ff275d3f4aa45f6b3064a31a6a
simtrami <me@simtrami.net>
2025-04-26 17:59:50 +0200

    Update login page design with enhanced styling
    Revised the login page by improving background gradients, animation, and layout alignment for better aesthetics. Changed button styles for a cleaner and more modern look. These enhancements provide a more user-friendly and visually appealing interface.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,19 +1,22 @@
 <x-layout>
     <x-slot:title>Connexion - Chrono Friseur</x-slot:title>
 
-
-    <div class="relative bg-gradient-to-bl from-slate-50 via-indigo-50 to-indigo-200 bg-cover">
+    <div class="relative bg-white bg-cover">
         <div
-            class="min-h-screen flex flex-col align-middle sm:justify-center items-center pt-6 sm:pt-0 bg-dots"
-        >
-            <div class="motion-safe:animate-bounce drop-shadow-lg">
+            class="absolute bg-gradient-to-bl from-slate-50 via-indigo-100 to-indigo-300 animate-[pulse_5s_ease-in-out_infinite] w-full h-full"
+        ></div>
+
+        <div class="absolute bg-dots animate-pulse w-full h-full"></div>
+
+        <div class="flex flex-col items-center justify-center align-middle mx-4 min-h-screen">
+            <div class="drop-shadow-lg">
                 <h1 class="text-4xl font-light text-gray-900">
                     <span class="font-extrabold">Chrono</span> Friseur
                 </h1>
             </div>
 
             <a href="{{ route('auth.github') }}"
-               class="flex items-center justify-center w-full mt-6 px-6 py-4 bg-indigo-100/20 backdrop-blur-sm shadow-lg overflow-hidden rounded-md hover:bg-indigo-50/20 hover:shadow-2xl focus:outline-2 sm:max-w-md transition"
+               class="flex items-center justify-center w-full mt-6 px-6 py-4 bg-white/40 backdrop-blur-xs shadow-lg overflow-hidden rounded-md hover:bg-white/60 hover:shadow-2xl focus:outline-2 sm:max-w-md transition"
             >
                 <div class="flex items-center space-x-2 mx-2 p-2">
                     <x-icons.github size="size-8"/>
